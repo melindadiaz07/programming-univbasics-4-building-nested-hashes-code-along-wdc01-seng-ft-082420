566896211ecc405c4b4a3de5eaf7b776fc5c19a7
Melinda Diaz <melindadiaz@me.com>
2020-07-19 21:40:42 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: "lord Montague",
+        name: "Lord Montague",
         age: "53"
         },
       :matriarch => {
