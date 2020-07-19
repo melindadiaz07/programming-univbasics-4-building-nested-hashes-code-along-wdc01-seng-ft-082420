72b362a7cfc98db9313e7560547bad87d912a8f7
Melinda Diaz <melindadiaz@me.com>
2020-07-19 21:34:07 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,38 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     montogue: {
-       patriarch: {
-         name: "Lord Montague",
-         age: "53"
-       },
-       matriarch: {
-         name: "Lady Montague",
-         age: "54"
-       },
-       hero: {
-         name: "Romeo",
-         age: "15"
-       },
-       hero_friends: [
-         {
-           name: "Benvolio",
-           age: "17",
-           attitude: "worried"
-         },
-         {
-           name: "Mercutio",
-           age: "18",
-           attitude: "hot-headed"
-         }
-         ]
-     },
-     capulet: {
-       patriarch: {},
-       matriarch: {},
-       heroine: {},
-       heroin_friends: []
-       
-     }
-     
-    }
+ montague: {},
+ capulet: {}
+}
 end
